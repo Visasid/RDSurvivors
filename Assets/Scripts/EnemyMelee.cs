25d03bb1b686ae8b3ede1e8cb5dd16fc7adf2b4d
Visasid <malyshev.0513mail.ru@gmail.com>
2023-04-01 00:45:53 +0500

    Added slime death animation

--- Assets/Scripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyMelee.cs
@@ -9,27 +9,37 @@ public class EnemyMelee : MonoBehaviour
     public int damage;
 
     private Transform player;
+    private Animator anim;
     private Rigidbody2D rb;
+    private float deathTimer;
+    private bool isDead = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player").transform;
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         Vector2 velocity = new Vector2((transform.position.x - player.position.x) * moveSpeed, (transform.position.y - player.position.y) * moveSpeed).normalized;
-        rb.velocity = -velocity;
+        if (!isDead) rb.velocity = -velocity;
+        else if (isDead && deathTimer >= 1) Destroy(gameObject);
+        if (isDead) deathTimer += Time.deltaTime;
     }
     private void OnCollisionStay2D(Collision2D other)
     {
-        if (other.transform.CompareTag("Player")) other.gameObject.GetComponent<PlayerMovement>().GetDamage(damage);
+        if (other.transform.CompareTag("Player") && !isDead) other.gameObject.GetComponent<PlayerMovement>().GetDamage(damage);
     }
 
     public void GetDamage(int dmg)
     {
         health -= dmg;
-        if (health <= 0) Destroy(gameObject);
+        if (health <= 0)
+        {
+            anim.SetTrigger("Death");
+            isDead = true;
+        }
     }
 }
