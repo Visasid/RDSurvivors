cf969f6c82bfb2a2dc1001dc563d8446413ec83a
Visasid <malyshev.0513mail.ru@gmail.com>
2023-03-31 18:18:34 +0500

    Fixed hand rotation, bullet now flies, ammo system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public int damage;
 
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
--- Assets/Scripts/HandRotation.cs
+++ Assets/Scripts/HandRotation.cs
@@ -8,19 +8,8 @@ public class HandRotation : MonoBehaviour
 
     private void Update()
     {
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-        //Ta Daaa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + offset));
-    }
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        Vector3 diference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotateZ = Mathf.Atan2(diference.y, diference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + offset);
     }
 }
