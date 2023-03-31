010be2794e3a46bce63fa4f49004f9ce95405bb0
Visasid <malyshev.0513mail.ru@gmail.com>
2023-03-31 18:25:40 +0500

    Added reloading

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
-    [SerializeField] private int maxAmmo;
-    [SerializeField] private int damage;
-    [SerializeField] private float shotCD;
+    public int maxAmmo;
+    public int damage;
+    public float shotCD;
+    public float reloadTime;
     [SerializeField] private Transform shotPoint;
 
     public float ammo;
     private float shotTimer = 100;
+    private float reloadTimer = 0;
+    private bool isReloading = false;
 
     private void Start()
     {
@@ -22,14 +25,23 @@ public class Gun : MonoBehaviour
     {
         //Timers
         shotTimer += Time.deltaTime;
+        if (isReloading) reloadTimer += Time.deltaTime;
+        if (reloadTimer >= reloadTime)
+        {
+            reloadTimer = 0;
+            isReloading = false;
+            ammo = maxAmmo;
+        }
 
         //Script
-        if (Input.GetMouseButton(0) && shotTimer >= shotCD && ammo > 0)
+        if (Input.GetMouseButton(0) && shotTimer >= shotCD && ammo > 0 && !isReloading)
         {
             shotTimer = 0;
             Bullet bul = Instantiate(bullet, shotPoint.position, shotPoint.rotation).GetComponent<Bullet>();
             bul.damage = damage;
             ammo -= 1;
         }
+
+        if (Input.GetKeyDown(KeyCode.R) && !isReloading && ammo != maxAmmo) isReloading = true;
     }
 }
