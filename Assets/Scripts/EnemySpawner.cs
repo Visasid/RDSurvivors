6f37da52d74783852adf3553a9cfb80fcc349cfd
Visasid <malyshev.0513mail.ru@gmail.com>
2023-04-02 13:52:06 +0500

    Added timer to UI

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject min5Enemy;
+    [SerializeField] private TMP_Text timerUI;
 
     private float spawnRate = 3;
-
     private float timerS = 0;
     private float timerM = 0;
     private float spawnT = 3;
@@ -34,6 +35,8 @@ public class EnemySpawner : MonoBehaviour
             timerS = 0;
             if (timerM > 3 && timerM < 5) spawnRate = 2;
         }
+
+        timerUI.text = timerM.ToString() + ':' + timerS.ToString();
     }
 
     private void SpawnEnemy(GameObject enemy)
